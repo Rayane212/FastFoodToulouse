791a829bc5f5c3e91f49f4ede3ed436cc850593f
MNBRH <nadirbrh31@outlook.fr>
2024-03-20 14:04:22 +0100

    Mise en place du filtrage des restaurants

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Filter from '../components/Filter';
+import RestaurantList from '../components/RestaurantList';
 
-const Home = () => {
+const HomePage = () => {
+    const initialRestaurants = [
+        { id: 1, name: 'Restaurant 1', location: 'centre-ville', specialty: 'burgers', origin: 'italien' },
+        { id: 2, name: 'Restaurant 2', location: 'peripherie', specialty: 'kebabs', origin: 'libanais' },
+        { id: 3, name: 'Restaurant 3', location: 'centre-ville', specialty: 'pizzas', origin: 'italien' },
+  
+    ];
+
+    const [restaurants, setRestaurants] = useState(initialRestaurants);
+
+    const handleFilterChange = (filters) => {
+       
+        let filteredRestaurants = initialRestaurants;
+
+        if (filters.location) {
+            filteredRestaurants = filteredRestaurants.filter(restaurant => restaurant.location === filters.location);
+        }
+        if (filters.specialty) {
+            filteredRestaurants = filteredRestaurants.filter(restaurant => restaurant.specialty === filters.specialty);
+        }
+        if (filters.origin) {
+            filteredRestaurants = filteredRestaurants.filter(restaurant => restaurant.origin === filters.origin);
+        }
+
+      
+        setRestaurants(filteredRestaurants);
+    };
+
     return (
         <div>
-            <h1>Home</h1>
+            <Filter onFilterChange={handleFilterChange} />
+            <RestaurantList restaurants={restaurants} />
         </div>
     );
 };
 
-export default Home;
+export default HomePage;
