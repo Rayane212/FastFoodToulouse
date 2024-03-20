02a444a7e792f6c79f0fdc4917ddad1663cd3dda
MNBRH <nadirbrh31@outlook.fr>
2024-03-20 14:26:28 +0100

    Ajouter les types de propriétés pour les composants Filter et RestaurantList

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const RestaurantList = ({ restaurants }) => {
     return (
@@ -18,6 +19,10 @@ const RestaurantList = ({ restaurants }) => {
             </ul>
         </div>
     );
+};
+
+RestaurantList.propTypes = {
+    restaurants: PropTypes.array.isRequired,
 };
 
 export default RestaurantList;
