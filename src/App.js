f74be2baafe44bc00f7cedf63872fe2fddc76182
Rayane Hadi <rayanehadi41@gmail.com>
2024-03-20 13:47:47 +0100

    Header with toggle button user admin mode

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import HomePage from './pages/HomePage';
 import FormPage from './pages/FormPage';
 import RestaurantPage from './pages/RestaurantPage';
 import Error404 from './pages/Error404';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
+        <Header></Header>
         {/* Ici, tu peux ajouter des composants qui restent constants, comme Header et Footer */}
         <Routes>
           <Route path="/" element={<HomePage />} />
