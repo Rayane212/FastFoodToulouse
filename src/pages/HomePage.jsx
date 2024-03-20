6fc7bd59ef67fd594c54d754f1d96f8414f50490
Rayane Hadi <rayanehadi41@gmail.com>
2024-03-20 11:21:11 +0100

    init some page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,41 +1,11 @@
-import React, { useState } from 'react';
-import Filter from '../components/Filter';
-import RestaurantList from '../components/RestaurantList';
+import React from 'react';
 
-const HomePage = () => {
-    const initialRestaurants = [
-        { id: 1, name: 'Restaurant 1', location: 'centre-ville', specialty: 'burgers', origin: 'italien' },
-        { id: 2, name: 'Restaurant 2', location: 'peripherie', specialty: 'kebabs', origin: 'libanais' },
-        { id: 3, name: 'Restaurant 3', location: 'centre-ville', specialty: 'pizzas', origin: 'italien' },
-  
-    ];
-
-    const [restaurants, setRestaurants] = useState(initialRestaurants);
-
-    const handleFilterChange = (filters) => {
-       
-        let filteredRestaurants = initialRestaurants;
-
-        if (filters.location) {
-            filteredRestaurants = filteredRestaurants.filter(restaurant => restaurant.location === filters.location);
-        }
-        if (filters.specialty) {
-            filteredRestaurants = filteredRestaurants.filter(restaurant => restaurant.specialty === filters.specialty);
-        }
-        if (filters.origin) {
-            filteredRestaurants = filteredRestaurants.filter(restaurant => restaurant.origin === filters.origin);
-        }
-
-      
-        setRestaurants(filteredRestaurants);
-    };
-
+const Home = () => {
     return (
         <div>
-            <Filter onFilterChange={handleFilterChange} />
-            <RestaurantList restaurants={restaurants} />
+            <h1>Home</h1>
         </div>
     );
 };
 
-export default HomePage;
+export default Home;
