b94a6b1c47b22d7dd49ebcb5355259f9f27cb9d1
Rayane Hadi <rayanehadi41@gmail.com>
2024-03-20 11:21:11 +0100

    init some page

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -1,27 +1,11 @@
-import React, { useState } from 'react';
-import Filter from '../components/Filter';
-import RestaurantList from '../components/RestaurantList';
+import React from 'react';
 
-const HomePage = () => {
-    const [restaurants, setRestaurants] = useState([
-        { id: 1, name: 'Restaurant 1', location: 'centre-ville', specialty: 'burgers', origin: 'italien' },
-        { id: 2, name: 'Restaurant 2', location: 'peripherie', specialty: 'kebabs', origin: 'libanais' },
-        { id: 3, name: 'Restaurant 3', location: 'centre-ville', specialty: 'pizzas', origin: 'italien' },
-        
-    ]);
-
-    const handleFilterChange = (filters) => {
-        // Implémentez la logique pour filtrer les restaurants en fonction des filtres sélectionnés
-        // Mettez à jour la liste des restaurants en conséquence
-        console.log('Filtres sélectionnés:', filters);
-    };
-
+const FormPage = () => {
     return (
         <div>
-            <Filter onFilterChange={handleFilterChange} />
-            <RestaurantList restaurants={restaurants} />
+
         </div>
     );
 };
 
-export default HomePage;
+export default FormPage;
